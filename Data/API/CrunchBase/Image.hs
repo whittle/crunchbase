749fd85594303d66eddd4afd692bd45000c3c212
Jason Whittle <jason.whittle@gmail.com>
2012-05-15 19:13:55 -0400

    Clean up some helper functions in the Image module.

--- Data/API/CrunchBase/Image.hs
+++ Data/API/CrunchBase/Image.hs
@@ -25,11 +25,11 @@ data AvailableSize = AvailableSize { width :: Number
 
 instance FromJSON AvailableSize where
   parseJSON (Array a) = return $ AvailableSize
-                        { width = unpackHead . V.head $ a
-                        , height = unpackLast . V.head $ a
+                        { width = unpackNumber . unpackHead . V.head $ a
+                        , height = unpackNumber . unpackLast . V.head $ a
                         , url = unpackString . V.last $ a
                         }
-    where unpackHead (Array v) = unpackInteger . V.head $ v
-          unpackLast (Array v) = unpackInteger . V.last $ v
-          unpackInteger (Number n) = n
+    where unpackHead (Array v) = V.head v
+          unpackLast (Array v) = V.last v
+          unpackNumber (Number n) = n
           unpackString (String t) = t
